62a4ebefd393214d96596c36dab5f14062b31eb8
Jorge G <jorge.gar@me.com>
2020-08-02 19:37:27 -0700

    Add launch screen and sleep for 1.5 seconds. Change icons and added small logo on launch screen

--- tip_calc/tip_calc/AppDelegate.swift
+++ tip_calc/tip_calc/AppDelegate.swift
@@ -15,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // This is my launchscreen time
+        Thread.sleep(forTimeInterval: 1.5)
         return true
     }
 
